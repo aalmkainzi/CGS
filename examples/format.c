d72dba10e45eaf2ce9af82ca73178585bc0ce301
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2026-04-12 09:31:13 +0300

    format example

--- examples/format.c
+++ examples/format.c
@@ -5,6 +5,9 @@ int main()
 {
     char buf[64];
     
-    cgs_format(buf, "%? %?%?", 1, 2, 3);
+    cgs_format(buf, "%? + %? = %?", 2, 3, 5);
+    cgs_println(buf);
+    
+    cgs_format(buf, "%1 + %0 = %2", 2, 3, 5);
     cgs_println(buf);
 }
