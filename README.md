1eca004347d7664a782c583d7a66ddd1b089010c
aalmkainzi <aa.almkainzi@wakeb.tech>
2026-09-01 12:30:34 +0300

    tostr example, and README changes

--- README.md
+++ README.md
@@ -256,7 +256,7 @@ struct FOO {
 
 CGS_Error foo_to_str(CGS_Writer *dst, struct FOO f, CGS_StrView fmt_arg)
 {
-    CGS_Error err = cgs_putc(dst, f.n);
+    CGS_Error err = cgs_appendf(dst, "%? :: %?", f.n, fmt_arg);
     return err;
 }
 
@@ -269,6 +269,12 @@ int main()
 {
     struct FOO f = {'a'};
     cgs_printf("%?", f); // can now use `struct FOO` in contexts that require a type with tostr
+    
+    cgs_printf("%(this is the fmt arg)", f); // prints "a :: this is the fmt arg"
+    
+    // syntax to using the fmt_arg with positional arguments:
+    struct FOO f2 = {'b'};
+    cgs_printf("%[0(for apple)], %[1(for banana)]", f, f2); // prints "a :: for apple, b :: for banana"
 }
 ```
 
--- examples/tostr.c
+++ examples/tostr.c
@@ -4,9 +4,9 @@ struct FOO {
     char n;
 };
 
-CGS_Error foo_to_str(CGS_Writer *dst, struct FOO foo, CGS_StrView fmt_arg)
+CGS_Error foo_to_str(CGS_Writer *dst, struct FOO f, CGS_StrView fmt_arg)
 {
-    CGS_Error err = cgs_putc(dst, foo.n);
+    CGS_Error err = cgs_appendf(dst, "%? :: %?", f.n, fmt_arg);
     return err;
 }
 
@@ -17,6 +17,12 @@ CGS_Error foo_to_str(CGS_Writer *dst, struct FOO foo, CGS_StrView fmt_arg)
 
 int main()
 {
-    struct FOO f = { 'a' };
-    cgs_printf("%?\n", f);
+    struct FOO f = {'a'};
+    cgs_printfln("%?", f); // can now use `struct FOO` in contexts that require a type with tostr
+    
+    cgs_printfln("%(this is the fmt arg)", f); // prints "a :: this is the fmt arg"
+    
+    // syntax to using the fmt_arg with positional arguments:
+    struct FOO f2 = {'b'};
+    cgs_printfln("%[0(for apple)], %[1(for banana)]", f, f2); // prints "a :: for apple, b :: for banana"
 }
